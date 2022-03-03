9e631000456d9e188fd6e44a8333f23b54c6d075
Jackson Augusto <58312218+jack-Danw@users.noreply.github.com>
2022-03-03 20:26:06 -0300

    made two additional tabs on the app.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'teste01',
+  appName: 'Ecotopia_United',
   webDir: 'www',
   bundledWebRuntime: false
 };
